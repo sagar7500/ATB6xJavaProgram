a5cd1cc939702a7ca5def58ba822ca53de3fed33
Sagar Garg <gargsagar026@gmail.com>
2024-04-27 17:48:45 +0530

    second commit

--- src/Lab001.java
+++ src/Lab001.java
@@ -3,7 +3,8 @@ public class Lab001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("abc");
+			System.out.println("Hello World");
+		
 		
 	}
 
