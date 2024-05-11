dc742e6a15182fc4f196f5de0bf96d24e5e34173
Sagar Garg <gargsagar026@gmail.com>
2024-05-11 19:18:02 +0530

    commit lab004

--- src/ex_11042024/Lab004.java
+++ src/ex_11042024/Lab004.java
@@ -3,7 +3,7 @@ package ex_11042024;
 public class Lab004 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Interview Q");
 		// public static void main(String[]--
 		// This structure is fix any change in this will not be identified by java
 		
